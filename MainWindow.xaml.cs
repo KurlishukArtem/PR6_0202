37d6cf5f491ece811cbe4d30fa41eecf31ace6ff
student-a502 <student-a502@PC-A502-11>
2024-12-19 13:21:30 +0500

    Программирование окна mainWindow. Kurlishuk

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegIN_Kurlishuk.Classes;
 
 namespace RegIN_Kurlishuk
 {
@@ -20,9 +22,33 @@ namespace RegIN_Kurlishuk
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(new Pages.Login());
         }
+        public void OpenPage(Page page)
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityMaskProperty, StartAnimation);
+
+            };
+            frame.BeginAnimation(Frame.OpacityMaskProperty, StartAnimation);
+
+        }
+
     }
 }
